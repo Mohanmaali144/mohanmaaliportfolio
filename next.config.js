e9bb53c42f2c196a865107492d016cc172636386
Mohan Maali <121346790+Mohanmaali144@users.noreply.github.com>
2025-06-01 17:41:31 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,24 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    appDir: true, // Enable App Router
+    appDir: true, // Enable the `app` directory
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http', // Allow http protocol
+        hostname: '**', // Allow any hostname
+        port: '', // Leave empty for no specific port
+        pathname: '/**', // Match all paths
+      },
+      {
+        protocol: 'https', // Allow https protocol
+        hostname: '**', // Allow any hostname
+        port: '', // Leave empty for no specific port
+        pathname: '/**', // Match all paths
+      },
+    ],
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
